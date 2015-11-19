80db529fae588f1e79405035ca188227811c80d2
Marimout <truong.quocanh@outlook.com>
2015-11-20 00:07:56 +0100

    Test should_call_protected_method (doesn't work)

---
+++ ClassLibrary1/OneClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMoq
+{
+    public class OneClass : AbstractClass
+    {
+        protected override void OneProtectedMethod()
+        {
+            Console.WriteLine("Hello");
+        }
+
+        public void DoMethod()
+        {
+            OneProtectedMethod();
+        }
+    }
+
+    public abstract class AbstractClass
+    {
+        protected abstract void OneProtectedMethod();
+    }
+}
---
+++ ClassLibrary1/TestOneClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NUnit.Framework;
+using Moq;
+using Moq.Protected;
+
+namespace TestMoq
+{
+    [TestFixture]
+    public class TestOneClass
+    {
+        [Test]
+        public void should_call_protected_method()
+        {
+            var mock = new Mock<OneClass>();
+
+            mock.Protected().Setup("OneProtectedMethod").Callback(() => Console.WriteLine("Goodbye"));
+
+            var o = new OneClass();
+
+            o.DoMethod();
+
+            mock.Protected().Verify("OneProtectedMethod", Times.Once());
+        }
+    }
+}
